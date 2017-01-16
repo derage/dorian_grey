26d07b55e183ab2301391efdee5916987756ecf9
jcafarelli <jcafarelli@roamdata.com>
2017-01-16 12:33:32 -0500

    Adds devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ require 'api_constraints'
 
 
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json },
             constraints: { subdomain: 'api' }, path: '/'  do
